8e0ccc4be2b6e477dc7251c329100f4d6128dec9
akhila2903 <144839214+akhila2903@users.noreply.github.com>
2023-10-31 12:40:47 -0400

    Update script.js

--- Kuncha-Akhilasai_hw3-secondPhaseOfApp/script.js
+++ Kuncha-Akhilasai_hw3-secondPhaseOfApp/script.js
@@ -1,161 +1,32 @@
+function submitForm() {
+    // Get form values
+    const currentLocation = document.getElementById("currentLocation").value;
+    const desiredLocation = document.getElementById("desiredLocation").value;
+    const currentResidence = document.getElementById("currentResidence").value;
+    const hobbies = document.getElementById("hobbies").value;
+    const languages = document.getElementById("languages").value;
+    const skills = document.getElementById("skills").value;
+    const education = document.getElementById("education").value;
+    const interests = document.getElementById("interests").value;
+    const placeOfBirth = document.getElementById("placeOfBirth").value;
+    const userPicture = document.getElementById("userPicture");
+    const downloadResume = document.getElementById("downloadResume");
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDbhNxMty_3eu8e3nVyHkYcmERmrn4RpWE",
-  authDomain: "first-webapp-8aef8.firebaseapp.com",
-  projectId: "first-webapp-8aef8",
-  storageBucket: "first-webapp-8aef8.appspot.com",
-  messagingSenderId: "63376560026",
-  appId: "1:63376560026:web:d3ef8e2ce56a1687aaf423",
-  measurementId: "G-EP8ZEFZZVG"
-  };
+    // Display user details
+    userPicture.src = URL.createObjectURL(document.getElementById("picture").files[0]);
+    downloadResume.href = URL.createObjectURL(document.getElementById("resume").files[0]);
 
-// intialize firebase
-firebase.initializeApp(firebaseConfig);
+    // Toggle additional facts display
+    const showFacts = document.getElementById("showFacts");
+    const additionalFacts = document.getElementById("additionalFacts");
 
-// reference for my data base
-var contactFormDB = firebase.database().ref('contactForm');
-
-document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-
-function submitForm(e){
-    e.preventDefault();
-    var name = getElementVal('name');
-    var email = getElementVal('emailid');
-    var location = getElementVal('location');
-    var msgContent = getElementVal('msgContent');
-
-    console.log(name,email,location,msgContent);
-    saveMessages(name,email,location,msgContent);
-
-    document.querySelector('.alert').style.display = 'block';
-
-    setTimeout(() =>{
-        document.querySelector('.alert').style.display = 'none';
-    }, 2000);
-    
-    document.getElementById('contactForm').reset();
-}
-
-const saveMessages = (name,email,location,msgContent) => {
-    var newContactForm = contactFormDB.push();
-    newContactForm.set({
-        name:name,
-        email:email,
-        location:location,
-        msgContent: msgContent
-    });
-};
-
-
-const getElementVal = (id) => {
-    return document.getElementById(id).value;
-};
-const userForm = document.getElementById('userForm');
-const getSuggestionButton = document.getElementById('getSuggestionButton');
-const suggestion = document.getElementById('suggestion');
-document.addEventListener('DOMContentLoaded', function () {
-    // Geolocation to get the current location
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            const currentLocation = document.getElementById('currentLocation');
-            currentLocation.value = `Lat: ${position.coords.latitude}, Long: ${position.coords.longitude}`;
-        });
+    if (showFacts.checked) {
+        additionalFacts.style.display = "block";
+    } else {
+        additionalFacts.style.display = "none";
     }
 
-    // Use a free IP Geolocation API to approximate user's location
-    fetch("https://ipinfo.io/json")
-        .then(response => response.json())
-        .then(data => {
-            const fromLocation = document.getElementById('fromLocation');
-            fromLocation.value = data.city + ", " + data.country;
-        });
-
-    // Use a free API or other data source to populate other facts (e.g., desired location)
-    fetch("https://api.example.com/desiredLocation")
-        .then(response => response.json())
-        .then(data => {
-            const desiredLocation = document.getElementById('desiredLocation');
-            desiredLocation.value = data.city + ", " + data.country;
-        });
-    
-userForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    // Simulated suggestion based on user's provided information
-    const currentLocation = document.getElementById('currentLocation').value;
-    const fromLocation = document.getElementById('fromLocation').value;
-    const desiredLocation = document.getElementById('desiredLocation').value;
-    const familyLocation = document.getElementById('familyLocation').value;
-    const favDestinationInput = document.getElementById('favDestination');
-    const travelExperienceInput = document.getElementById('travelExperience');
-    const pictureInput = document.getElementById('picture');
-    const userPicture = document.getElementById('userPicture');
-
-
-
-
-pictureInput.addEventListener('change', function () {
-    if (pictureInput.files && pictureInput.files[0]) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            userPicture.src = e.target.result;
-        };
-        reader.readAsDataURL(pictureInput.files[0]);
-    }
-});
-
-
-
-    // Replace this with your actual logic to suggest destinations based on user data
-    const suggestedDestination = `How about visiting ${desiredLocation}? It's a beautiful place to explore. Enjoy your trip as a ${selectedGender}.`;
-
-        // Display the suggestion
-        suggestion.textContent = suggestedDestination;
-    });
-
-
-const portfolioContainer = document.getElementById('portfolio');
-
-function displayPortfolios() {
-    portfolios.forEach((portfolio) => {
-        const portfolioSection = document.createElement('div');
-        portfolioSection.className = 'portfolio-section';
-        const portfolioTitle = document.createElement('h2');
-        portfolioTitle.textContent = portfolio.name;
-        portfolioSection.appendChild(portfolioTitle);
-
-        portfolio.investments.forEach((investment) => {
-            const totalValue = investment.shares * investment.price;
-            const investmentCard = document.createElement('div');
-            investmentCard.className = 'investment-card';
-            const logoImage = document.createElement('img');
-            logoImage.className = 'logo';
-            logoImage.src = brandLogos[investment.symbol];
-            logoImage.alt = `${investment.symbol} Logo`;
-
-            const stockSymbol = document.createElement('p');
-            stockSymbol.innerHTML = `<strong>Stock Symbol:</strong> ${investment.symbol}`;
-
-            const shares = document.createElement('p');
-            shares.innerHTML = `<strong>Shares:</strong> ${investment.shares}`;
-
-            const pricePerShare = document.createElement('p');
-            pricePerShare.innerHTML = `<strong>Price per Share:</strong> $${investment.price}`;
-
-            const totalValueParagraph = document.createElement('p');
-            totalValueParagraph.innerHTML = `<strong>Total Value:</strong> $${totalValue}`;
-
-            investmentCard.appendChild(logoImage);
-            investmentCard.appendChild(stockSymbol);
-            investmentCard.appendChild(shares);
-            investmentCard.appendChild(pricePerShare);
-            investmentCard.appendChild(totalValueParagraph);
-            portfolioSection.appendChild(investmentCard);
-        });
-
-        portfolioContainer.appendChild(portfolioSection);
-    });
+    // Handle form submission
+    // You can send the form data to the server or perform any desired action here
+    // For example, you can use AJAX to send data to the server.
 }
-});
-

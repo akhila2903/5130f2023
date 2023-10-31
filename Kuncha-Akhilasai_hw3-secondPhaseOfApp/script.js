0be4701c6209ac54386efc82cb891f3a42c38514
akhila2903 <144839214+akhila2903@users.noreply.github.com>
2023-10-31 13:19:07 -0400

    Update script.js

--- Kuncha-Akhilasai_hw3-secondPhaseOfApp/script.js
+++ Kuncha-Akhilasai_hw3-secondPhaseOfApp/script.js
@@ -1,32 +1,31 @@
 function submitForm() {
-    // Get form values
-    const currentLocation = document.getElementById("currentLocation").value;
-    const desiredLocation = document.getElementById("desiredLocation").value;
-    const currentResidence = document.getElementById("currentResidence").value;
-    const hobbies = document.getElementById("hobbies").value;
-    const languages = document.getElementById("languages").value;
-    const skills = document.getElementById("skills").value;
-    const education = document.getElementById("education").value;
-    const interests = document.getElementById("interests").value;
-    const placeOfBirth = document.getElementById("placeOfBirth").value;
-    const userPicture = document.getElementById("userPicture");
-    const downloadResume = document.getElementById("downloadResume");
+            
+            document.getElementById("Message").style.display = "block";
+            document.getElementById("currentLocation").value = "";
+            document.getElementById("desiredLocation").value = "";
+            document.getElementById("currentResidence").value = "";            
+            document.getElementById("hobbies").value = "";
+            document.getElementById("languages").value = "";
+            document.getElementById("skills").value = "";
+            document.getElementById("education").value = "";
+            document.getElementById("interests").value = "";
+            document.getElementById("picture").value = "";
+            document.getElementById("resume").value = "";
+            document.getElementById("showFacts").value = "";
+            userPicture.src = URL.createObjectURL(document.getElementById("picture").files[0]);
+            downloadResume.href = URL.createObjectURL(document.getElementById("resume").files[0]);
 
-    // Display user details
-    userPicture.src = URL.createObjectURL(document.getElementById("picture").files[0]);
-    downloadResume.href = URL.createObjectURL(document.getElementById("resume").files[0]);
 
-    // Toggle additional facts display
-    const showFacts = document.getElementById("showFacts");
-    const additionalFacts = document.getElementById("additionalFacts");
 
-    if (showFacts.checked) {
-        additionalFacts.style.display = "block";
+        }
+const showFactsCheckbox = document.getElementById("showFacts");
+const additionalFactsDiv = document.getElementById("additionalFacts");
+
+showFactsCheckbox.addEventListener("change", function () {
+    if (showFactsCheckbox.checked) {
+        additionalFactsDiv.style.display = "block";
     } else {
-        additionalFacts.style.display = "none";
+        additionalFactsDiv.style.display = "none";
     }
-
-    // Handle form submission
-    // You can send the form data to the server or perform any desired action here
-    // For example, you can use AJAX to send data to the server.
-}
+});
+    

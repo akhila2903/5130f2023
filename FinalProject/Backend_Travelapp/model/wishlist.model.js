f5f453b72824475d2f64ba638610d3b7f8e99c1a
akhila2903 <144839214+akhila2903@users.noreply.github.com>
2023-12-11 01:04:30 -0500

    Delete FinalProject/Backend_Travelapp directory

--- FinalProject/Backend_Travelapp/model/wishlist.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const wishlistSchema = new mongoose.Schema({
-    hotelId: {type:String, require:true}
-})
-
-const Wishlist = mongoose.model("Wishlist", wishlistSchema);
-
-module.exports = Wishlist;
